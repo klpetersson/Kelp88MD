ed976f2000baf19fda89de122009553f4408b168
klpetersson <klpetersson@gmail.com>
2014-08-10 13:06:36 +0200

    testingCommit

---
+++ Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  Kelp88MD
+//
+//  Created by Kent Petersson on 06/08/14.
+//  Copyright (c) 2014 Kent Petersson. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Event: NSManagedObject {
+
+    @NSManaged var timeStamp: NSDate
+
+}
